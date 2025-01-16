4928d8c7d0ebb720fc915e464cc3c763db456345
David G <48044383+gradav@users.noreply.github.com>
2025-01-16 17:29:48 -0600

    Update providers.tf

--- providers.tf
+++ providers.tf
@@ -1,13 +1,1 @@
-# Declare GOOGLE_CREDENTIALS as a variable
-variable "GOOGLE_CREDENTIALS" {
-  type        = string
-  description = "The JSON credentials for the Google Cloud provider."
-  sensitive   = true
-}
 
-provider "google" {
-  project = "terraform-445119"
-  region  = "us-central1"
-  zone    = "us-central1-a"
-  credentials = var.GOOGLE_CREDENTIALS
-}
